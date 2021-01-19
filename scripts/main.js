4b9bc5421e71875be5a6f45a3b0d535b8fc8ea85
abx393 <abandari99@gmail.com>
2021-01-18 19:36:25 -0800

    experimenting

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
-console.log(process.env)
+//require('dotenv').config();
 
-/*
 require(['dotenv'], function(dotenv) {
     dotenv.config();
     console.log(process.env)
 });
-*/
 
 const interval = 5; // Sampling period
 const period = 5000; // Period of cube's rotation
